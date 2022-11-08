7513e9c5d06c7d531c71dc79663180cf767420ef
Adrian WEAVER <aweaver@student.42.fr>
2022-11-08 14:46:31 +0100

    FIX MERGE ISSUE:
    Forgot to remove some merge tokens in previous file

--- hook_strlen.sh
+++ hook_strlen.sh
@@ -3,13 +3,7 @@
 ret=0
 tst=0
 
-<<<<<<< HEAD:run.sh
-printf '%b\n' "$(cat pimp.txt)"
-
-bash run_tests.sh
-=======
 bash run_tests.sh strlen
->>>>>>> 4a26504 (ADD HOOK STRLEN:):hook_strlen.sh
 tst=$?
 
 if [ $tst -ne 0 ]; then
