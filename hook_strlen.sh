4d8c9ce70759f7adf3d6a7efeaf5caa108d57241
Adrian WEAVER <aweaver@student.42.fr>
2022-11-08 10:41:24 +0100

    ADD HOOK STRLEN:
    Adding hook file for strlen.

--- hook_strlen.sh
+++ hook_strlen.sh
@@ -3,9 +3,13 @@
 ret=0
 tst=0
 
+<<<<<<< HEAD:run.sh
 printf '%b\n' "$(cat pimp.txt)"
 
 bash run_tests.sh
+=======
+bash run_tests.sh strlen
+>>>>>>> 4a26504 (ADD HOOK STRLEN:):hook_strlen.sh
 tst=$?
 
 if [ $tst -ne 0 ]; then
@@ -14,9 +18,8 @@ if [ $tst -ne 0 ]; then
 
 fi
 
-norminette -R CheckForbiddenSourceHeader srcs/ | grep -v "Warning: Not a valid file"
-ret=$?
-
+norminette srcs/ft_strlen.c 2>/dev/null
+tst=$?
 if [ $tst -ne 0 ]; then
 
     ret=1
